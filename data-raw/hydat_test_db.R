928c0ab6e47b10f7f972052029c4cf52cf2aaae6
Sarah Popov <sarah.popov@gmail.com>
2022-11-23 19:35:47 -0800

    Update test db with DBI methods

--- data-raw/hydat_test_db.R
+++ data-raw/hydat_test_db.R
@@ -9,12 +9,12 @@ hydat_load()
 if(!is_hydat(hydat_get_db())) stop("Hydat database wasn't loaded!")
 
 db <- hydat_get_db()
-db_heads <- sapply(src_tbls(db), function(x) tbl(db, x) %>% head(1000) %>% collect())
+db_heads <- sapply(DBI::dbListTables(db), function(x) tbl(db, x) %>% head(1000) %>% collect())
 
 outfile <- "data-raw/Hydat_sqlite3_99999999.db"
 if(file.exists(outfile)) unlink(outfile)
-src_test <- src_sqlite(outfile, create = TRUE)
-src_test_out <- lapply(src_tbls(db), function(x) {
+src_test <- DBI::dbConnect(RSQLite::SQLite(), outfile)
+src_test_out <- lapply(DBI::dbListTables(db), function(x) {
   copy_to(src_test, db_heads[[x]], name = x, temporary = FALSE)
 })
 
